e580ea5bcd0d5791b01f558fbe50f426c1f3a0f6
RafeStefano <stefanorafe@gmail.com>
2021-03-10 22:49:22 +0300

    added todo list model and template

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField(auto_now_add=True)
+    complated = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
 
 def home(request):
     return render(request, 'todo/home.html')
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render (request, 'todo/todo_list.html', context)
